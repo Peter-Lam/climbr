9347b1a6561f78667306d18be19c85bccb64751d
Peter Lam <peter.quach.lam@gmail.com>
2020-08-02 15:51:43 -0400

    Normalized start and end times for booking scraper, minor folder restructure

--- web_scraper/bookings.py
+++ web_scraper/bookings.py
@@ -5,15 +5,15 @@
 # --------------------------------
 
 import argparse
-import datetime
 import pathlib
 import os
 import platform
 import sys
+import re
 import requests
 sys.path.append('../')
 import common.common as common
-
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver import Chrome
 
@@ -36,8 +36,8 @@ def main():
             raise Exception(f"{platform.system()} is not supported")
         # Declaring variables
         webpage = "https://app.rockgympro.com/b/widget/?a=offering&offering_guid=90a6abd5e5124f7384b2b60d00683e3d&random=5f1483d0c152d&iframeid=&mode=p"
-        current_time = str(datetime.datetime.now())
-        year = str(datetime.datetime.now().year)
+        current_time = str(datetime.now())
+        year = str(datetime.now().year)
 
         # Using Selenium to read the website
         chrome_options = webdriver.ChromeOptions()
@@ -54,18 +54,25 @@ def main():
 
         # Parsing data
         __, date, time = time_slot.split(',')
-        start, end = time.split('to')
+        start = time.split('to')[0].strip()
+        end = time.split('to')[1].strip()
+        # If format doesn't match HH:MM AM/PM format then change it to that
+        pattern = re.compile("^(1[0-2]|0[1-9]):[0-5][0-9] (AM|PM)$")
+        if not pattern.match(start):
+            start = datetime.strftime(datetime.strptime(start, "%I %p"), "%I:%M %p")
+        if not pattern.match(end):
+            end = datetime.strftime(datetime.strptime(end, "%I:%M %p"), "%I:%M %p")
         availability = int(availability.replace('Availability', '').replace('spaces', '').strip('\n').strip())
         booking = {'date': date.strip(),
                    'year': year,
                    'time_slot': time.strip(),
-                   'start_time': start.strip(),
-                   'end_time': end.strip(),
+                   'start_time': start,
+                   'end_time': end,
                    'availability': availability,
                    'retrieved_at': current_time}
         # Logging and saving info
-        common.write_log(f"Retrieved session at {current_time}", os.path.join(PROJECT_DIR, 'bookings.log'))
-        common.update_bulk_api(booking, os.path.join(os.path.dirname(PROJECT_DIR), 'elasticsearch_files','bookings.json'), 'bookings')
+        common.write_log(f"Retrieved session at {current_time}", os.path.join(os.path.dirname(PROJECT_DIR), 'logs', 'bookings.log'))
+        common.update_bulk_api(booking, os.path.join(os.path.dirname(PROJECT_DIR), 'data','output','bookings.json'), 'bookings')
 
     except Exception as ex:
         # driver.quit()
