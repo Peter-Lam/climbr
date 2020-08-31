13dc9502debbd7b8bb0dc6b0577d043fb741f1f3
Peter Lam <peter.quach.lam@gmail.com>
2020-08-31 13:55:19 -0400

    Added Update and Show features to main climb.py script

--- common/constants.py
+++ common/constants.py
@@ -1,15 +1,6 @@
 '''
 This module contains contants used by various scripts within this project
 '''
-
-LOCATIONS = {'altitude_kanata': {'lat': '45.297970', 'lon': '-75.911150'},
-             'altitude_gatineau': {'lat': '45.446861', 'lon': '-75.736801'},
-             'hogs_back': {'lat': '45.3710517', 'lon': '-75.698022'},
-             'calabogie': {'lat': '45.264209', 'lon': '-76.813545'},
-             'lac_beauchamp': {'lat': '45.490288', 'lon': '-75.617274'}}
-
-ALTITUDE_SCALE = ['VB/V0', 'V0/V1', 'V1/V2', 'V2/V3',
-                  'V3/V4', 'V4/V5', 'V5/V6', 'V6/V7', 'V7/V8', 'V8/V9', 'V9+']
 V_SCALE = ['VB', 'V0-', 'V0', 'V0+', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6',
            'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17']
 FONT_SCALE = ['3', '4-', '4', '4+', '5', '5+', '6A', '6A+', '6B', '6B+', '6C', '6C+',
