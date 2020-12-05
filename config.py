0e1269049ad242783ca367347603d03a48b19da2
Peter Lam <peter.quach.lam@gmail.com>
2020-12-05 12:31:51 -0500

    Python Notebook added, Weather is now being tracked, and booking crontab has been updated to reflect recent Coyote time changes - [Added] weather.py to gather weather data for Kanata and Gatineau - [Added] Python notebook to analyze online booking trends associated with weather - [Added] Python notebook to analyze common words within session logs - [Added] Weather data stored in csv for Gatineau and Kanata - [Added] New sample images from notebook - [Added] Config file for API key for https://www.visualcrossing.com/ - [Bug Fix] Templates had 'V5/6' instead of 'V5/V6' - [Updated] crontab with new booking times and scheduled API calls for weather - [Updated] bookings.json with more data - [Updated] requirements.txt

---
+++ config.py
@@ -0,0 +1,3 @@
+# API key for https://www.visualcrossing.com/
+# Used for weather data on bookings
+weather_api = None
--- web_scraper/utils/update_data.py
+++ web_scraper/utils/update_data.py
@@ -6,6 +6,8 @@
 
 import os
 import sys
+import pandas as pd
+from datetime import datetime
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(BASE_DIR)
@@ -13,16 +15,46 @@ import common.globals as glbs  # noqa
 import common.common as common  # noqa
 
 
-def main():
+def get_bookings():
+    '''
+    Retreive booking data and return the data
+    '''
     file = os.path.join(glbs.ES_BULK_DATA, 'bookings.json')
-    bookings = common.load_bulk_json(file)
-    temp = 0
-    for row in bookings:
-        # Make adjustments to row data here
-        row['start_minute'] = int(common.str_to_time(row['start_time']).minute)
-    common.write_bulk_api(bookings, os.path.join(
-        glbs.ES_BULK_DATA, 'bookings.json'), 'bookings')
-    print("Update complete")
+    return common.load_bulk_json(file)
+
+
+def update():
+    '''
+    Edit this function to update the existing json file
+    '''
+    # # Get data
+    # bookings = get_bookings()
+    # for row in bookings:
+    #     # Set dataframe depending on the location of the booking
+    #     df = ott if row['location'] in [
+    #         'Altitude Kanata', 'Coyote Rock Gym'] else gat
+    #     # reformat the date from json
+    #     date = datetime.fromisoformat(
+    #         row['retrieved_at']).strftime(("%Y-%m-%d"))
+    #     # Look for booking data with weather, if it doesn't match then ignore
+    #     weather = df.loc[df['date'] == date]
+    #     if weather.shape[0] >= 1:
+    #         for col in list(weather):
+    #             if weather.shape[0] > 1:
+    #                 print("Warning: More than one value found for weather, using last")
+    #                 row[col] = weather.iloc[-1][col]
+    #             else:
+    #                 row[col] = weather.iloc[0][col]
+    #     else:
+    #         print(f"No weather data found for {date}")
+    #         pass
+    # common.write_bulk_api(bookings, os.path.join(
+    #     glbs.ES_BULK_DATA, 'bookings22.json'), 'bookings')
+    # print("Update complete")
+
+
+def main():
+    update()
 
 
 if __name__ == "__main__":
