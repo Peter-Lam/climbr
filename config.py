c7d791112125b34ebd922d5955f37939a5b31afc
Peter Lam <peter.quach.lam@gmail.com>
2021-09-23 21:07:48 -0400

    Updated config

--- config.py
+++ config.py
@@ -1,4 +1,18 @@
+import os
+import common.globals as glbs
+import common.common as common
 # API key for https://www.visualcrossing.com/
 # Used for weather data on bookings
+# Can be left empty if features are not needed
 weather_key = ''
 firestore_json = ''
+# Email credentials used to send SMTP requests when errors occur. Uses ENV variables
+# Can be left empty if email notification is not needed
+smpt_email = os.getenv('CLIMBR_EMAIL')
+smpt_pass = os.getenv('CLIMBR_PASS')
+
+# Email to send notifications to
+to_notify = ''
+
+# Home gym
+home_gym = ''
